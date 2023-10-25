80a3d092d393abbabd115ff00dd55f90d6d23ac9
Kuba <jakub.wykocki@wp.pl>
2023-10-25 20:04:19 +0200

    added wiremock and rest template

--- src/main/java/com/joboffers/domain/offer/OfferFetchable.java
+++ src/main/java/com/joboffers/domain/offer/OfferFetchable.java
@@ -3,7 +3,10 @@ package com.joboffers.domain.offer;
 import java.util.Arrays;
 
 import com.joboffers.domain.offer.dto.JobOfferResponse;
+import org.springframework.context.annotation.Bean;
+
 import java.util.List;
+
 
 public interface OfferFetchable {
     List<JobOfferResponse> fetchOffers();
