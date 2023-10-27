8801d74c3c3da037c815a589166076f0affb7554
Kuba <jakub.wykocki@wp.pl>
2023-10-27 20:57:25 +0200

    mongodb and repository added

--- src/main/java/com/joboffers/domain/offer/OfferFacadeConfiguration.java
+++ src/main/java/com/joboffers/domain/offer/OfferFacadeConfiguration.java
@@ -2,49 +2,26 @@ package com.joboffers.domain.offer;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 
 @Configuration
+@AllArgsConstructor
 public class OfferFacadeConfiguration {
 
     @Bean
-    OfferFacade offerFacade(OfferFetchable offerFetchable) {
-        OfferRepository repo = new OfferRepository() {
-            @Override
-            public boolean existsByOfferUrl(String offerUrl) {
-                return false;
-            }
+    OfferFacade offerFacade(OfferFetchable offerFetchable, OfferRepository repository) {
 
-            @Override
-            public Optional<Offer> findByOfferUrl(String offerUrl) {
-                return Optional.empty();
-            }
-
-            @Override
-            public List<Offer> saveAll(List<Offer> offers) {
-                return null;
-            }
-
-            @Override
-            public List<Offer> findAll() {
-                return null;
-            }
-
-            @Override
-            public Optional<Offer> findById(String id) {
-                return Optional.empty();
-            }
-
-            @Override
-            public Offer save(Offer offer) {
-                return null;
-            }
-        };
-        OfferService offerService = new OfferService(offerFetchable, repo);
-        return new OfferFacade(repo, offerService);
+        OfferService offerService = new OfferService(offerFetchable, repository);
+        return new OfferFacade(repository, offerService);
     }
 }
