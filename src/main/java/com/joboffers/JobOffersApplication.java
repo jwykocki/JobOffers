c4d0df830a4032a685029904bc70c5d429508cbe
Kuba <jakub.wykocki@wp.pl>
2023-11-09 15:26:28 +0100

    deleted unused classes

--- src/main/java/com/joboffers/JobOffersApplication.java
+++ src/main/java/com/joboffers/JobOffersApplication.java
@@ -1,16 +1,14 @@
 package com.joboffers;
 
 
-import com.joboffers.infrastructure.offer.http.OfferHttpClientConfigurationProperties;
 import com.joboffers.infrastructure.security.jwt.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {OfferHttpClientConfigurationProperties.class, JwtConfigurationProperties.class})
+@EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class JobOffersApplication {
 
     public static void main(String[] args) {
