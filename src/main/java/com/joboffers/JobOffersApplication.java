7d355fa3b7b8128d1004ac3cbfad5f154d212d52
Kuba <jakub.wykocki@wp.pl>
2023-12-30 12:33:27 +0100

    deployment on aws

--- src/main/java/com/joboffers/JobOffersApplication.java
+++ src/main/java/com/joboffers/JobOffersApplication.java
@@ -2,16 +2,19 @@ package com.joboffers;
 
 
 import com.joboffers.infrastructure.security.jwt.JwtConfigurationProperties;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
+@Log4j2
 @EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class JobOffersApplication {
 
     public static void main(String[] args) {
+        log.info("--- Starting application ---");
         SpringApplication.run(JobOffersApplication.class, args);
     }
 
